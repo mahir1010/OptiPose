851c40bc8cb50157e68f62f7e280f65af8503a09
Mahir Patel <mahir1010@gmail.com>
2022-10-21 12:54:53 -0400

    update requirements, added training examples

---
+++ examples/model_train_r3d.py
@@ -0,0 +1,92 @@
+# Pre-generated datasets are provided in the augmented_train_test folder on the Google Drive.
+# Usage python model_train_r3d.py n_cm k n_heads
+
+import csv
+import json
+import pickle
+
+import numpy as np
+import tensorflow as tf
+
+from OptiPose import OptiPoseConfig
+from OptiPose.models.metrics.MPJPE import build_mpjpe_metric
+from OptiPose.models.metrics.PCK import build_pck_metric
+from OptiPose.models.postural_autoencoder import optipose_postural_autoencoder
+from OptiPose.models.utils import train, build_spatio_temporal_loss
+
+R3D_config = OptiPoseConfig('./example_configs/Rodent3D.yml')
+
+import sys
+
+num_samples = 20000
+numPCMs = int(sys.argv[1])
+num_sub_ck = int(sys.argv[2])
+dataset_title = "Rodent3D"
+multi_heads = int(sys.argv[3])
+n_kps = R3D_config.num_parts  # 10 parts now including SP1 and SP2
+batch_size = 100
+dataset = {'input': [], 'label': []}
+vdataset = {'input': [], 'label': []}
+
+model = optipose_postural_autoencoder(30, n_kps, numPCMs, num_sub_ck, multi_heads, output_dim=60)
+# model.summary()
+
+metrics = ["mae", build_pck_metric(n_kps, 0.05), build_pck_metric(n_kps, 0.1), build_mpjpe_metric()]
+
+dataset = {'input': [], 'label': []}
+vdataset = {'input': [], 'label': []}
+reader = csv.reader(
+    open(f'./augmented_train_test/R3D/IJCV_{dataset_title}_dataset_t_attention_{30}_{num_samples}.csv'),
+    delimiter='|')
+
+# Test file will still contain 6500 samples
+reader1 = csv.reader(
+    open(f'./augmented_train_test/R3D/IJCV_{dataset_title}_dataset_t_attention_{30}_{num_samples}_test.csv'),
+    delimiter='|')
+
+next(reader)
+next(reader1)
+
+for row in reader:
+    dataset["input"].append(np.array(json.loads(row[0])))
+    dataset["label"].append(np.array(json.loads(row[1])))
+
+for row in reader1:
+    vdataset["input"].append(np.array(json.loads(row[0])))
+    vdataset["label"].append(np.array(json.loads(row[1])))
+xTrain = dataset['input']
+yTrain = dataset['label']
+xVal = vdataset['input']
+yVal = vdataset['label']
+
+train_dataset = tf.data.Dataset.from_tensor_slices((xTrain, yTrain)).batch(batch_size).shuffle(15000,
+                                                                                               reshuffle_each_iteration=True)
+validation_dataset = tf.data.Dataset.from_tensor_slices((xVal, yVal)).batch(batch_size)
+
+
+def scheduler(epoch, lr):
+    if epoch % 40 == 0 and epoch < 1000:
+        return lr * 0.95
+    elif epoch % 300 == 0 and epoch < 3000:
+        return lr * 0.95
+    elif epoch % 300 == 0:
+        return lr * 0.95
+    else:
+        return lr
+
+
+callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
+
+checkpoint_path = f"./Model_weights/R3D/optipose-{numPCMs}-{num_sub_ck}-{dataset_title}-{multi_heads}" + "/closs-{epoch:04d}.ckpt"
+callback_1 = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path,
+    verbose=0,
+    save_weights_only=True,
+    save_best_only=True,
+    mode='max',
+    monitor='val_pck_5')
+
+history = train(model, train_dataset, validation_dataset, build_spatio_temporal_loss(0.0001, 0.0001), 2000, 3.1e-4,
+                metrics, [callback, callback_1])
+with open(f'optipose-{numPCMs}-{num_sub_ck}-{dataset_title}-{multi_heads}-history', 'wb') as file_pi:
+    pickle.dump(history.history, file_pi)
