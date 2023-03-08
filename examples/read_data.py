9a59fd42ed6bd8b5175808b1c0c0df8e80b5e194
Mahir Patel <mahirp@durip.bu.edu>
2023-03-08 16:13:44 -0500

    added synchronized depth video code

--- examples/read_data.py
+++ examples/read_data.py
@@ -2,7 +2,8 @@ from OptiPose.data_store_interface import initialize_datastore_reader
 from OptiPose.video_reader_interface import initialize_video_reader
 from OptiPose import OptiPoseConfig
 import matplotlib.pyplot as plt
-import cv2
+import h5py
+import cv2,numpy as np
 
 config = OptiPoseConfig('./example_configs/Rodent3D.yml')
 
@@ -14,15 +15,21 @@ for index,skeleton in datastore.row_iterator():
     break
 
 #Load Video
-video_reader = initialize_video_reader('<video_path>',60,'opencv')
+video_reader = initialize_video_reader('<video file path>',60,'opencv')
 #Start reading
 video_reader.start()
 plt.ion()
+#Load Depth
+data_file = h5py.File('<corresponding depth hdf5 path>','r')
+dataset = data_file['depth'] #Select depth group
 while True:
-    frame = video_reader.next_frame()
-    frame_number = video_reader.get_current_index()
+    frame = video_reader.next_rame() #Read Frame
+    frame_number = video_reader.get_current_index() #Get Frame number
     if frame is None:
         break
+    depth_image = dataset[frame_number] #Read corresponding depth frame
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.1), cv2.COLORMAP_JET) # Map depth values to valid RGB range
+    images = np.hstack((frame, depth_colormap)) #Stack frames side by side
     print(frame_number)
-    plt.imshow(frame)
+    plt.imshow(images) #Show frames
     plt.pause(1/60)
